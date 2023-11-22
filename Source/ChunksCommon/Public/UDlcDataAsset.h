320eefdd07b708c08126c97e3038f1cac0878629
Rafasloth <91292632+rafasloth@users.noreply.github.com>
2023-11-21 23:33:02 -0600

    part-11: Adds a custom Data Asset inherited class for use in /Game/DLC/DLC_X/Directory.uasset

---
+++ Source/ChunksCommon/Public/UDlcDataAsset.h
@@ -0,0 +1,33 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "UDlcDataAsset.generated.h"
+
+USTRUCT()
+struct FDlcInfo {
+    GENERATED_BODY()
+
+    UPROPERTY(EditAnywhere)
+    FString AssetName;
+
+    UPROPERTY(EditAnywhere)
+    UTexture2D* AssetThumbnail;
+
+    UPROPERTY(EditAnywhere)
+    FString MountPoint;
+
+    UPROPERTY(EditAnywhere)
+    FString LevelName = "Init";
+};
+
+
+UCLASS()
+class UDlcDataAsset : public UDataAsset {
+    GENERATED_BODY()
+
+    UPROPERTY(EditAnywhere)
+    TArray<FDlcInfo> AssetItems;
+};
